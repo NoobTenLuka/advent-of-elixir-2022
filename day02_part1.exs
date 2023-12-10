c620878861834894f889db1aed78eda9fe06f5ed
NoobTenLuka <NoobTenLuka@gmail.com>
2023-12-10 19:26:43 +0100

    Add day 2 part 1

---
+++ day02_part1.exs
@@ -0,0 +1,18 @@
+IO.puts(
+  File.read!("inputs/02-input.txt")
+  |> String.split("\n", trim: true)
+  |> Stream.map(fn line ->
+    [enemy, mine] =
+      String.split(line, " ") |> Enum.map(fn x -> x |> String.to_charlist() |> hd end)
+
+    mine = mine - 88
+    enemy = enemy - 65
+
+    case Integer.mod(enemy - mine, 3) do
+      0 -> 3
+      1 -> 0
+      2 -> 6
+    end + mine + 1
+  end)
+  |> Enum.sum()
+)
