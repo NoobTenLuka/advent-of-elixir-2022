f9b9198e608efb9fb0251c9b0d38b0d490d56c12
NoobTenLuka <NoobTenLuka@gmail.com>
2023-12-12 12:27:39 +0100

    Change output to use pipe operator

--- day01_part1.exs
+++ day01_part1.exs
@@ -1,8 +1,7 @@
-IO.puts(
-  File.read!("inputs/01-input.txt")
-  |> String.split("\n\n")
-  |> Stream.map(fn elve ->
-    elve |> String.trim() |> String.split("\n") |> Enum.map(&String.to_integer(&1)) |> Enum.sum()
-  end)
-  |> Enum.max()
-)
+File.read!("inputs/01-input.txt")
+|> String.split("\n\n")
+|> Stream.map(fn elve ->
+  elve |> String.trim() |> String.split("\n") |> Enum.map(&String.to_integer(&1)) |> Enum.sum()
+end)
+|> Enum.max()
+|> IO.puts()
